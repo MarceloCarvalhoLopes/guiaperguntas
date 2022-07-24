43cad222e37a3a338052a14e8e372423e4a3f3a7
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-24 15:15:06 -0300

    Showing variables on the HTML

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 //Informando o Express user o EJS como view engine
 app.set('view engine','ejs');
 
-app.get("/",(req, res) => {
-    res.render("main/perfil");
+app.get("/:nome/:lang",(req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        inscritos:8000
+    });
 });
 
 app.listen(8080,() => {
