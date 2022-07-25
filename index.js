bd92898ec017fa1349467bed0a57c56db9cfd96c
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-25 09:39:02 -0300

    static files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 //Informando o Express user o EJS como view engine
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get("/:nome/:lang",(req, res) => {
     var nome = req.params.nome;
