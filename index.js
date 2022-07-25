a4af7a89a579b02e82b21e26bc9a11c5d9f42708
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-25 13:17:36 -0300

    installing the Bootstrap

--- index.js
+++ index.js
@@ -5,25 +5,9 @@ const app = express();
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
+app.get("/",(req, res) => {
 
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca Cola", preco: 5.14},
-        {nome: "Leite", preco: 8.14}
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos:8000,
-        msg: exibirMsg,
-        produtos:produtos
-    });
+    res.render("index");
 });
 
 app.listen(8080,() => {
