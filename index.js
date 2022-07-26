7c14cb8ed6dc48846a896c6c8387fb52487cbd8b
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-25 21:21:37 -0300

    sending data to backend

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 
 //Informando o Express user o EJS como view engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+//rotas
 app.get("/",(req, res) => {
 
     res.render("index");
@@ -15,7 +22,10 @@ app.get("/perguntar",(req, res) =>{
 });
 
 app.post("/salvarpergunta",(req,res) =>{
-    res.send("Formulário recebido!");
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send("Formulário recebido! titulo " + titulo + " " + "descricao " + descricao);
 });
 
 app.listen(8080,() => {
