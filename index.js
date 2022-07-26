98280131a583a5dc8d529e77c34c06aa7a0f34b7
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-26 09:44:33 -0300

    connection with database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
+//database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com banco de dados!")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);
+    })
 
 //Informando o Express user o EJS como view engine
 app.set('view engine','ejs');
