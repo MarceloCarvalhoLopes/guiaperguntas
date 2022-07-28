18373ae957ad98f5adc2f16afe21d3536f127df9
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-07-28 18:49:18 -0300

    Front-End question page

--- index.js
+++ index.js
@@ -59,7 +59,9 @@ app.get("/pergunta/:id",(req,res) => {
         where: {id: id}
     }).then(pergunta =>{  //pegunta achada
         if(pergunta != undefined){
-            res.render("pergunta");
+            res.render("pergunta",{
+                pergunta: pergunta 
+            });
         }else{ //pergunta não encontrada
             res.redirect("/");
         }
